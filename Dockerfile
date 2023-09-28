1ed59790c9ce50904e647c727eecd4ac921f1ce9
Aayush <agcdev0@gmail.com>
2023-09-28 12:15:55 +0000

    Working

--- Dockerfile
+++ Dockerfile
@@ -5,6 +5,6 @@ ENV PYTHONDONTWRITEBYTECODE 1
 
 # RUN apt-get update && apt-get install -y build-essentials
 
-RUN pip install flask celery redis
+RUN pip install flask celery redis==4.5.2
 
 WORKDIR /app
--- app/myapp/__init__.py
+++ app/myapp/__init__.py
@@ -1,19 +1,20 @@
 from celery import Celery
 from celery.contrib.abortable import AbortableTask
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
+from celery.result import AsyncResult
 from time import sleep
 
 def make_celery(app):
     celery = Celery(app.name)
     celery.conf.update(app.config["CELERY_CONFIG"])
-
-    class ContextTask(celery.Task):
-        def __call__(self, *args, **kwargs):
-            with app.app_context():
-                return self.run(*args, **kwargs)
-
-    celery.Task = ContextTask
     return celery
+
+class TestException(Exception):
+    pass
+
+def raise_exception(sleep_interval):
+    sleep(sleep_interval)
+    raise TestException('this function raises an exception')
 
 def create_app():
     app = Flask(__name__)
@@ -27,8 +28,17 @@ def create_app():
     @celery.task(bind=True, base=AbortableTask)
     def count(self):
         for i in range(10):
-            print("Hello World" + i)
-            sleep(1)
+            if i !=8:
+                print("Hello World", i)
+                self.update_state(state='PROGRESS', meta={'current': i, 'total': 10})
+                sleep(1)
+            else:
+                try:
+                    raise_exception(2)
+                except TestException as exc:
+                    self.update_state(state='FAILURE', meta={'exc_type':type(exc).__name__})
+                    print('Caught exception: {}'.format(exc))
+                    raise                
         return 'DONE!' 
 
     @app.route('/start')
@@ -41,5 +51,19 @@ def create_app():
     def cancel(task_id):
         celery.control.revoke(task_id, terminate=True)  # Use the celery instance to access control
         return 'Canceled!'
+
+    @app.route('/check_task/<task_id>')
+    def check_task(task_id):
+        task = AsyncResult(task_id, app=celery)
+        if task.state == 'PROGRESS':
+            progress = task.info.get('current', 0)
+            total = task.info.get('total', 40)
+            return jsonify({'status': 'PROGRESS', 'progress': progress, 'total': total})
+        elif task.state == 'SUCCESS':
+            return jsonify({'status': 'SUCCESS'})
+        elif task.state == 'FAILURE':
+            return jsonify({'status': 'FAILURE'})
+        else:
+            return jsonify({'status': 'PENDING'})
 
     return app, celery
